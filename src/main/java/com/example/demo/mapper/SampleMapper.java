64e9142a741c514a21743aafd8cc8e0e8bac6916
kaito kuroba <57478299+3118659412@users.noreply.github.com>
2021-12-02 21:58:56 +0800

    add sample

--- src/main/java/com/example/demo/mapper/SampleMapper.java
+++ src/main/java/com/example/demo/mapper/SampleMapper.java
@@ -1,6 +1,9 @@
 package com.example.demo.mapper;
 
+import com.example.demo.entity.Sample;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * @author kaito kuroba
@@ -9,4 +12,39 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface SampleMapper {
+
+    /**
+     * 获取单个序列
+     * @param sampleId
+     * @return
+     */
+    Sample get(Integer sampleId);
+
+    /**
+     * 获取所有序列
+     * @return
+     */
+    List<Sample> gets();
+
+    /**
+     * 删除单个序列
+     * @param sampleId
+     * @return
+     */
+    Integer del(Integer sampleId);
+
+    /**
+     * 更改序列信息
+     * @param sample
+     * @return
+     */
+    Integer update(Sample sample);
+
+    /**
+     * 增加序列信息
+     * @param sample
+     * @return
+     */
+    Integer add(Sample sample);
+
 }
